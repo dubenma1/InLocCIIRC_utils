fix overflow bug in projectMesh

projectMesh/demo.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from projectMesh import projectMesh
 
 meshPath = '/Volumes/GoogleDrive/Můj disk/ARTwin/InLocCIIRC_dataset/models/B-670/mesh_rotated.obj'
-f = 600
+f = 1385.6406460551023
 R = np.eye(3)
 t = np.transpose(np.array([0.0, 1.0, 0.0]))
 sensorSize = np.array([1600, 1200])

projectMesh/projectMesh.py
@@ -105,7 +105,7 @@ if __name__ == '__main__':
     f = float(inputData['f'])
     R = inputData['R']
     t = inputData['t']
-    sensorSize = inputData['sensorSize']
+    sensorSize = inputData['sensorSize'].astype(np.int64) # avoid overflow, because they are uint16 by default
     ortho = inputData['ortho']
     mag = inputData['mag']
 